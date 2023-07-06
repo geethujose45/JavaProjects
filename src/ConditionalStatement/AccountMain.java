e67f6f37ab083e0e16520e50bcc824559aa97027
geethujose45 <geethujoseca@gmail.com>
2023-07-05 20:28:17 -0400

    Commit testing

--- src/ConditionalStatement/AccountMain.java
+++ src/ConditionalStatement/AccountMain.java
@@ -7,17 +7,17 @@ public class AccountMain {
 	public static void main(String[] args) {
 		Account ac = new Account();
 		Scanner sc = new Scanner(System.in);
-		ac.balanceAmount=7850.45;
-		//ac.witdrawAmount=10000;
-		ac.overdraftAmount=8000;
+		ac.balanceAmount = 7850.45;
+		// ac.witdrawAmount=10000;
+		ac.overdraftAmount = 8000;
 		System.out.println("Enter the pin:");
-		ac.enteredPin=sc.nextInt();
+		ac.enteredPin = sc.nextInt();
 		System.out.println("Enter the amount to withdraw");
-		ac.witdrawAmount=sc.nextInt();
+		ac.witdrawAmount = sc.nextInt();
 		ac.validatePINAndwithdrawCash();
 		ac.atmWithdrawal();
 		sc.close();
-
+		System.out.println("Testing gitbash commit");
 	}
 
 }
