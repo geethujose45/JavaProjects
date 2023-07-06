c54f693fd1e60f87e53516530de9756aebc5333b
geethujose45 <geethujoseca@gmail.com>
2023-07-05 21:13:35 -0400

    Github Push Test

---
+++ src/gitHubPushTest/gitHubTestPush.java
@@ -0,0 +1,12 @@
+package gitHubPushTest;
+
+public class gitHubTestPush {
+	int x;
+	int y;
+
+	void addTwoNumbers() {
+		int sum = x + y;
+		System.out.println("Addition of two number " + x + " and " + y + " Is " + sum);
+
+	}
+}
---
+++ src/gitHubPushTest/mainGitHubTestPush.java
@@ -0,0 +1,13 @@
+package gitHubPushTest;
+
+public class mainGitHubTestPush {
+
+	public static void main(String[] args) {
+		gitHubTestPush gp = new gitHubTestPush();
+		gp.x = 10;
+		gp.y = 20;
+		gp.addTwoNumbers();
+
+	}
+
+}
